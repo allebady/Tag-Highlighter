5389ad89dd120fc0c23a6a1eb8858c9e145e8616
vandenium <no@email.com>
2021-07-07 01:07:43 -0700

    Update tab layout to use flexbox for consistentcy across themes
    - use flexbox for tab layout to size all tabs evenly and make use of all horizontal space.
    - error message notification stretches 100%.

--- Emp++_Tag_Highlighter07.user.js
+++ Emp++_Tag_Highlighter07.user.js
@@ -128,7 +128,10 @@ function runScript(){
         "<div id='s-conf-wrapper'>" +
         "<h1>Empornium++Tag Highlighter Settings</h1>" +
         "<div id='s-conf-status'></div>" +
-        "<ul id='s-conf-tabs'>" +
+        
+        // Tabs
+        "<div class='tab-row-container'" +
+        "<ul class='tab-row'>" +
         "<li data-page='s-conf-general' class='s-selected'><a class='s-conf-tab' >General</a></li>" +
         "<li data-page='s-conf-good-tags'><a class='s-conf-tab'>Liked Tags</a></li>" +
 		"<li data-page='s-conf-loved-tags'><a class='s-conf-tab'>Loved Tags</a></li>" +
@@ -138,7 +141,11 @@ function runScript(){
 		"<li data-page='s-conf-amateur-tags'><a class='s-conf-tab'>Amateur Tags</a></li>" +
 		"<li data-page='s-conf-loveamat-tags'><a class='s-conf-tab'>Loved Amateur Tags</a></li>" +
 		"<li data-page='s-conf-maleperf-tags'><a class='s-conf-tab'>Male Performer Tags</a></li>" +
-		"<li data-page='s-conf-lovemale-tags'><a class='s-conf-tab'>Loved Male Performer Tags</a></li>" +
+        "</ul>" +
+        "</div>"+
+        "<div class='tab-row-container'" +
+        "<ul class='tab-row'>" +
+        "<li data-page='s-conf-lovemale-tags'><a class='s-conf-tab'>Loved Male Performer Tags</a></li>" +
 		"<li data-page='s-conf-likesite-tags'><a class='s-conf-tab'>Liked Site Tags</a></li>" +
 		"<li data-page='s-conf-lovesite-tags'><a class='s-conf-tab'>Loved Site Tags</a></li>" +
         "<li data-page='s-conf-disliked-tags'><a class='s-conf-tab'>Disliked Tags</a></li>" +
@@ -148,6 +155,8 @@ function runScript(){
         "<li data-page='s-conf-import-export'><a class='s-conf-tab'>Import/Export</a></li>" +
         "<li><a class='s-conf-tab' data-page=''></a></li>" +
         "</ul>" +
+        "</div>"+
+        // End Tabs
         "<div id='s-conf-content'>" +
         "<form id='s-conf-form'>" +
         "<div class='s-conf-page s-selected' id='s-conf-general'>" +
@@ -388,17 +397,19 @@ function runScript(){
         "#s-conf-wrapper{background:#eee; color:#444; position:relative; width:1200px; overflow:hidden; margin:50px auto; font-size:14px;" +
         "padding:15px 20px; border-radius:16px; box-shadow: 0 0 20px black;}" +
         "#s-conf-wrapper h2{background:none; text-align:left; color:#444; padding:0; border-radius: unset;}" +
-        "#s-conf-status{width:784px; padding:8px; line-height:16px; text-align:center; border:1px solid #ddd; margin-top:15px; display:none;}" +
+        "#s-conf-status{padding:8px; line-height:16px; text-align:center; border:1px solid #ddd; margin-top:15px; display:none;}" +
         "#s-conf-status.s-success{border-color:#135300; background:#A9DF9C;}" +
         "#s-conf-status.s-error{border-color:#840000; background:#F3AAAA;}" +
         "#s-conf-status-close{cursor:pointer;}" +
         "#s-conf-tabs{width:100%; margin:15px 0 -1px 0; overflow:hidden; cursor:pointer;}" +
         "#s-conf-tabs li, #s-conf-tabs h2{ border: 1px solid #444; border-bottom: 0; border-radius: 4px 4px 0 0; line-height: normal; width: 130px; margin:0; list-style:none;float:left;}" +
         "#s-conf-content{width:100%; overflow:hidden; border:1px solid #444; border-radius:4px; border-top-left-radius: 0px; box-shadow:0 -1px 10px rgba(0,0,0,0.6);}" +
-        ".s-conf-tab{height:40px; text-decoration: none; font-size:14px;display:block; text-align:center;" +
-        "color:#444;margin: 5px 2px 0px;}" +
-        "#s-conf-tabs li.s-selected, #s-conf-tabs li:hover {background-color:#fff;text-decoration:none;font-weight:bold; color: #333}" +
-        "#s-conf-tabs a:hover {text-decoration:none;font-weight:bold; color: #333}" +
+        ".tab-row-container {height: 50px;box-sizing: border-box;  display: flex;cursor:pointer;}" +
+        ".tab-row-container li {display:inline-block;height: 40px;flex: 1; list-style: none; margin: 0; border: 1px solid #444; border-bottom: 0; border-radius: 4px 4px 0 0; line-height: normal; text-align: center;padding:5px;}" +
+        ".tab-row-container a {color: #444; font-size:14px;text-align: center; text-decoration:none;}" +
+        ".tab-row-container a:hover { text-decoration:none; color: black;}" +
+        ".tab-row-container li:hover {background-color: white;}" +
+        ".tab-row-container li.s-selected {background-color:#fff;text-decoration:none; color:black}" +
         "#s-conf-form{display:block; background:#fff; padding:15px;}" +
         "#s-conf-form label{display:block;}" +
         ".s-conf-buttons{margin-top:8px; width:100%; text-align:center;}" +
@@ -946,7 +957,7 @@ function runScript(){
         $j(".s-conf-tab").parent().on("click", function(){
             var tab = $j(this);
             if(!tab.hasClass("s-selected")){
-                tab.siblings().removeClass('s-selected');
+                $j('.tab-row-container li').removeClass('s-selected');
                 $j('.s-conf-page').removeClass("s-selected");
                 tab.addClass("s-selected");
                 $j(".s-conf-page#" + tab.data("page")).addClass("s-selected");
