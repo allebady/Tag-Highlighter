1b20211768d90ceeb3dbebd5270d966b92906d51
allebady <32685112+allebady@users.noreply.github.com>
2021-10-02 02:11:08 +0200

    Update Emp++_Tag_Highlighter07.user.js

--- Emp++_Tag_Highlighter07.user.js
+++ Emp++_Tag_Highlighter07.user.js
@@ -59,7 +59,7 @@ function runScript(){
     var $j = $.noConflict(true);
 
     var defaults = {
-        majorVersion : 0.8,
+        majorVersion : 0.7,
         //Options
         truncateTags : true,
         //Browse Page Options
