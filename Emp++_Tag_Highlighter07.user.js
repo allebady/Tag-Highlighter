9bd684ef8ef657345a9fceec7ef2107985227311
vandenium <86036198+vandenium@users.noreply.github.com>
2021-10-10 15:06:56 -0700

    Set up default user tag groups in the default settings.
    - Set empty user tag groups in default settings.
    - Remove code importing old (<0.4) user tag group settings.

--- Emp++_Tag_Highlighter07.user.js
+++ Emp++_Tag_Highlighter07.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name Emp++ Tag Highlighter 0.7
-// @version 0.7.5c
+// @version 0.7.6
 // @description highlights liked/disliked tags
 // @grant GM_getValue
 // @grant GM_setValue
+// @grant GM_deleteValue
 // @require https://code.jquery.com/jquery-1.12.4.min.js
 // @match http://*.empornium.me/*
 // @match https://*.empornium.me/*
@@ -24,6 +25,9 @@
 // ==/UserScript==
 
 // Changelog:
+// Version 0.7.6
+// - Set empty user tag groups in default settings.
+// - Remove code importing old (<0.4) user tag group settings.
 // Version 0.7.5c
 // - Fixing some bugs that appear after the cleaning.
 // Version 0.7.5b
@@ -93,6 +97,25 @@ function runScript(){
 		buttonMaleperfTags : false,
 		buttonLikesiteTags : false,
 		buttonDislikedTags : false,
+
+		// User tag groups
+		tags: {
+			good : [],
+			loved : [],
+            performer : [],
+			loveperf : [],
+			newperf : [],
+			amateur : [],
+			loveamat : [],
+			maleperf : [],
+			lovemale : [],
+			likesite : [],
+			lovesite : [],
+            disliked : [],
+            hated : [],
+            terrible : [],
+            useless : [],
+		},
 	};
 
 	var settings = getSettings();
@@ -104,29 +127,6 @@ function runScript(){
 		saveSettings();
 		//handle upgrade
 	}
-
-    //import tags from pre-v0.4 ETH
-    if(!settings.tags){
-        settings.tags = {
-            good : getValue("good_tags", "").split(' '),
-			loved : getValue("loved_tags", "").split(' '),
-            performer : getValue("performer_tags", "").split(' '),
-			loveperf : getValue("loveperf_tags", "").split(' '),
-			newperf : getValue("newperf_tags", "").split(' '),
-			amateur : getValue("amateur_tags", "").split(' '),
-			loveamat : getValue("loveamat_tags", "").split(' '),
-			maleperf : getValue("maleperf_tags", "").split(' '),
-			lovemale : getValue("lovemale_tags", "").split(' '),
-			likesite : getValue("likesite_tags", "").split(' '),
-			lovesite : getValue("lovesite_tags", "").split(' '),
-            disliked : getValue("bad_tags", "").split(' '),
-            hated : getValue("hated_tags", "").split(' '),
-            terrible : getValue("terrible_tags", "").split(' '),
-            useless : getValue("useless_tags", "").split(' ')
-        };
-        saveSettings();
-    }	
-	
 	
 	var configHTML =
 		"<div id='s-conf-background'>" +
